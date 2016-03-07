97393560b56fca48cc01f459640218c8b6a39f27
freeman <the.freeman.lab@gmail.com>
2016-03-07 01:19:52 -0500

    add saturation style

--- fragment.glsl
+++ fragment.glsl
@@ -2,6 +2,12 @@ precision highp float;
 
 varying vec3 vnormal;
 
+struct Style {
+  float saturation;
+};
+
+uniform Style style;
+
 void main() {
-  gl_FragColor = vec4(vnormal, 0.5);
+  gl_FragColor = vec4(mix(abs(vnormal), vec3(1.0), 1.0 - style.saturation), 1.0);
 }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var glslify = require('glslify')
 
 module.exports = {
-  fragment: glslify('./fragment.glsl')
+  fragment: glslify('./fragment.glsl'),
+  uniforms: {
+    saturation: {type: 'float', default: 0.75}
+  }
 }
