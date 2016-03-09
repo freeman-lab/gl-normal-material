8af361ef3de044f738cffbf87409c316677cb917
freeman <the.freeman.lab@gmail.com>
2016-03-09 17:56:20 -0500

    rename

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var glslify = require('glslify')
 
 module.exports = {
   fragment: glslify('./fragment.glsl'),
-  uniforms: {
-    saturation: {type: 'float', default: 0.75}
-  }
+  style: {
+    saturation: {type: 'float', default: 0.75, min: 0, max: 1}
+  },
   name: 'normal'
 }
