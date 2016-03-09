dff8ef2fa5955dfac56cdb92e3c16e0eff722700
freeman <the.freeman.lab@gmail.com>
2016-03-09 18:41:30 -0500

    pluralize

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var glslify = require('glslify')
 
 module.exports = {
   fragment: glslify('./fragment.glsl'),
-  style: {
+  styles: {
     saturation: {type: 'float', default: 0.75, min: 0, max: 1}
   },
   name: 'normal'
