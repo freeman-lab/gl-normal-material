a1ef921f1fc34c4e6f58c2646bab755b1a29ceb2
freeman <the.freeman.lab@gmail.com>
2016-03-09 22:57:29 -0500

    add absolute value boolean flag

--- fragment.glsl
+++ fragment.glsl
@@ -4,10 +4,12 @@ varying vec3 vnormal;
 
 struct Style {
   float saturation;
+  bool absolute;
 };
 
 uniform Style style;
 
 void main() {
-  gl_FragColor = vec4(mix(abs(vnormal), vec3(1.0), 1.0 - style.saturation), 1.0);
+  vec3 color = (style.absolute == true) ? abs(vnormal) : max(vnormal, vec3(0.0, 0.0, 0.0));
+  gl_FragColor = vec4(mix(color, vec3(1.0), 1.0 - style.saturation), 1.0);
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var glslify = require('glslify')
 module.exports = {
   fragment: glslify('./fragment.glsl'),
   styles: {
-    saturation: {type: 'float', default: 0.75, min: 0, max: 1}
+    saturation: {type: 'float', default: 0.75, min: 0, max: 1},
+    absolute: {type: 'bool', default: true}
   },
   name: 'normal'
 }
